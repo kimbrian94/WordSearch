bdad653a0487f623f4f0e2c2938ae360e1da9cfb
Brian Kim <kim.brian94@gmail.com>
2020-08-26 15:53:23 -0400

    Initial Commit

---
+++ Word Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Search
+//
+//  Created by Brian Kim on 2020-08-26.
+//  Copyright © 2020 Brian Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
